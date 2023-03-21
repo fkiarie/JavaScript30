0f1f9ecd62c0e8f72ff287303259a482b3130479
fkiarie <kazungu61@outlook.com>
2023-03-21 15:16:04 +0700

    format the code
    format code for neatness

--- 01 - JavaScript Drum Kit/main.js
+++ 01 - JavaScript Drum Kit/main.js
@@ -1,17 +1,18 @@
-document.addEventListener('DOMContentLoaded', function () {
-    window.addEventListener('keydown', function (e) {
-        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-        const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-        if(!audio) return;
-        audio.currentTime = 0; // rewind audio to start
-        audio.play();
-        key.classList.add('playing');
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+    audio.currentTime = 0; // rewind audio
+    audio.play();
+    key.classList.add('playing');
+}
 
-        function removeTranstion(e){
-            if(e.propertyName !== 'transform') return;
-            this.classList.remove('playing');
-        }
-        const keys = document.querySelectorAll('.key');
-        keys.forEach(key => key.addEventListener('transitionend', removeTranstion));
-    });
-});
+function removeTranstion(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTranstion));
+window.addEventListener('keydown', playSound);
+
